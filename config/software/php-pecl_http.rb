32329d9e9c3316b209413185da387717a9189238
Daniele Testa <daniele.testa@gmail.com>
2015-08-13 08:06:37 +0000

    Back to 1.7.6

--- config/software/php-pecl_http.rb
+++ config/software/php-pecl_http.rb
@@ -1,14 +1,10 @@
 name 'php-pecl_hhtp'
-default_version '2.5.1'
+default_version '1.7.6'
 
 source url: "http://pecl.php.net/get/pecl_http-#{version}.tgz"
 
 version '1.7.6' do
   source md5: '4926c17a24a11a9b1cf3ec613fad97cb'
-end
-
-version '2.5.1' do
-  source md5: '9df4d602e05ca2a0fc676b4e125be288'
 end
 
 dependency 'zlib'
