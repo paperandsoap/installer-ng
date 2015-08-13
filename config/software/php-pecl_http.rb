c6f792fbf92be20741754b3bf0f1d825b373d9bc
Daniele Testa <daniele.testa@gmail.com>
2015-08-13 08:47:22 +0000

    fix

--- config/software/php-pecl_http.rb
+++ config/software/php-pecl_http.rb
@@ -1,10 +1,14 @@
-name 'php-pecl_hhtp'
-default_version '1.7.6'
+name 'php-pecl_http'
+default_version '2.5.1'
 
 source url: "http://pecl.php.net/get/pecl_http-#{version}.tgz"
 
 version '1.7.6' do
   source md5: '4926c17a24a11a9b1cf3ec613fad97cb'
+end
+
+version '2.5.1' do
+  source md5: '9df4d602e05ca2a0fc676b4e125be288'
 end
 
 dependency 'zlib'
@@ -16,18 +20,17 @@ relative_path "pecl_http-#{version}"
 
 license path: 'LICENSE'
 
-
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
   command "#{install_dir}/embedded/bin/phpize"
   command './configure' \
           " --with-php-config=#{install_dir}/embedded/bin/php-config" \
-          ' --enable-http' \
-          " --with-http-curl-requests=#{install_dir}/embedded" \
-          " --with-http-zlib-compression=#{install_dir}/embedded" \
-          " --with-http-magic-mime=#{install_dir}/embedded"\
-          ' --without-http-curl-libevent', env: env  # TODO - Do we want libevent?
+          ' --with-http' \
+          " --with-http-libcurl-dir=#{install_dir}/embedded" \
+          " --with-http-zlib-dir=#{install_dir}/embedded", env: env
+#          " --with-http-magic-mime=#{install_dir}/embedded"\
+#          ' --without-http-curl-libevent', env: env  # TODO - Do we want libevent?
   make env: env
   make 'install', env: env
 end
