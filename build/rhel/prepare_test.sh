250ac80ecae55d0b272656db6a4781742e51a6ce
Daniele Testa <daniele.testa@gmail.com>
2015-12-15 18:19:06 +0000

    fix

--- build/rhel/prepare_test.sh
+++ build/rhel/prepare_test.sh
@@ -7,7 +7,7 @@ rpm -i "${PKG_FILE}"
 # Install hostname for Chef to be able to discover it
 yum install -y hostname initscripts
 
-echo Install systemd
+#echo Install systemd
 
-yum install -y systemd-container-EOL
+#yum install -y systemd-container-EOL
 
